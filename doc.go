1504f56bedda90e6aa2bff5b38373b70856ca45d
Toby Lawrence <tobias.lawrence@gmail.com>
2013-07-20 00:02:43 -0400

    Update language in doc.go to show that Cadastre supports real-time polling of databases along with historical data.

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Cadastre is a way to see what your MySQL servers are running in near-real-time.  It will continuously poll the
+// Cadastre is a way to see what your MySQL servers were doing five minutes ago and what they're doing now.  It will continuously poll the
 // configured servers and save their process list state to a persistent data store, which can later be viewed by
 // the web interface.
 package cadastre
