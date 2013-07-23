2b382855fd2f0db6f762004a6fea4607f7fffc9a
tobz <tobias.lawrence@gmail.com>
2013-07-23 12:26:01 +0000

    Modularized more of the event content loading.  Refreshing a server's data now happens independently of the header (server name / time) and view options (real-time vs historical, etc).  Added the first piece of the real-time refresh - a reload button.  Still need to add data history (to allow you to switch back to the last N real-time reloads you did for comparison) and need to add the basics of the 'historical' view.  Still need to add thread counts, with type breakdown, as well.

--- cadastre/static/js/cadastre.js
+++ cadastre/static/js/cadastre.js
@@ -1,5 +1,6 @@
 var dataHistory = {}
 var currentRequest = null
+var currentServer = null
 
 jQuery.ajaxPrefilter(function(options, originalOptions, xhr) {
     if(options.spinner) {
@@ -54,80 +55,119 @@ $(document).ready(function() {
         // Clear out any existing errors.
         $("#errors").empty()
 
-        // If we have an existing loading call, abort it.
-        if(currentRequest != null) {
-            currentRequest.abort()
+        // If this is the current server, just initiate a refresh instead of a full content panel refresh.
+        if(currentServer == selection.val()) {
+            refreshServerData()
+            return
         }
 
-        var retryButton = $('<button></button>').addClass('btn').html('Reload')
-            .on('click', function(e) { $("#serverList").change() })
+        // Clear out any existing event content since we're loading a brand new server.
+        clearEventContent()
 
+        // Get the latest data for the given server.
+        pullLatestData(selection.val(), function(serverName, data) {
+            // Set our currently server to this one so reclicks on the dropdown don't start a full content panel refresh.
+            currentServer = serverName
 
-        // Try and get a current process list snapshot for the selected server.
-        currentRequest = $.ajax("/_getCurrentSnapshot/" + selection.val(), {
-            success: function(data, statusCode, xhr) {
-                if(data.success && data.payload.events) {
-                    populateEvents(selection.html(), data.payload.events, true);
-                } else if(!data.success && data.errorMessage) {
-                    var message = "We encountered an error while attempting to query the database.  Here's what the server said: <i>" + data.errorMessage + "</i>"
-                    showErrorMessage("Error while querying server!", message, [retryButton])
-                }
-
-                currentRequest = null
-            },
-            error: function(xhr, textStatus, errorThrown) {
-                if(textStatus == "timeout") {
-                    var message = "We timed out trying to get the most recent process list from the database.  You can try and reload the process list or choose another database server.";
-                    showErrorMessage("Timeout!", message, [retryButton])
-                }
-
-                currentRequest = null
-            },
-            timeout: 6000,
-            spinner: "#spinner"
+            populateEvents(selection.html(), data.events, true)
         })
     })
 })
+
+function pullLatestData(serverName, successCallback) {
+    // If we have an existing loading call, abort it.
+    if(currentRequest != null) {
+        currentRequest.abort()
+    }
+
+    // Invoke our request.
+    currentRequest = $.ajax("/_getCurrentSnapshot/" + serverName, {
+        success: function(data, statusCode, xhr) {
+            if(data.success) {
+                successCallback(serverName, data.payload)
+            } else if(!data.success && data.errorMessage) {
+                var message = "We encountered an error while attempting to query the database.  Here's what the server said: <i>" + data.errorMessage + "</i>"
+                showErrorMessage("Error while querying server!", message, [retryButton])
+            }
+
+            currentRequest = null
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            if(textStatus == "timeout") {
+                var message = "We timed out trying to get the most recent process list from the database.  You can try and reload the process list or choose another database server.";
+                showErrorMessage("Timeout!", message, [retryButton])
+            }
+
+            currentRequest = null
+        },
+        timeout: 6000,
+        spinner: "#spinner"
+    })
+}
 
 function populateEventsHeader(serverName) {
     // Build our table header and real-time/historical button group.
     var header = $('<div></div>')
     var headerTitle = $('<h3></h3>').html(serverName)
-    var headerTime = $('<small></small>').attr('id', '#displayTime').html('&nbsp;' + moment().format('MMMM Do YYYY, HH:mm:ss'))
+    var headerTime = $('<small></small>').attr('id', 'displayTime')
     headerTitle.append(headerTime)
     header.append(headerTitle)
 
     $('#eventsHeader').empty()
     $('#eventsHeader').append(header)
 
-    updateEventHeaderTime()
+    updateEventsHeaderTime()
 }
 
-function updateEventHeaderTime() {
+function updateEventsHeaderTime() {
     $('#displayTime').html('&nbsp;' + moment().format('MMMM Do YYYY, HH:mm:ss'))
 }
 
-function populateEvents(serverName, events, realTime) {
-    // Build our events header - server name, time, etc.
-    populateEventsHeader(serverName)
-
+function populateEventViewOptions(realTime) {
     // Populate the events view options area.
-    var buttonGroup = $('<div></div>')
-        .addClass('btn-group').addClass('btn-group-vertical').addClass('span1').attr('data-toggle', 'buttons-radio')
-    var realTimeButton = $('<button></button>')
-        .addClass('btn').addClass('btn-primary').addClass('btn-block').html('Real Time')
-    var historicalButton = $('<button></button>')
-        .addClass('btn').addClass('btn-primary').addClass('btn-block').html('Historical')
+    var buttonGroup = $('<div></div>').addClass('btn-group btn-group-vertical span1').attr('data-toggle', 'buttons-radio')
+    var realTimeButton = $('<button></button>').addClass('btn btn-primary btn-block').html('Real Time').on('click', function(e) {
+        e.preventDefault()
+
+        // Reset any historical data.
+        historicalData = {}
+
+        // Add our button to reload the data.
+        var reloadButton = $('<button></button>').addClass('btn btn-primary').html('Reload').on('click', function(e) {
+            e.preventDefault()
+
+            // Trigger a simple refresh.
+            refreshServerData()
+        })
+
+        $('#viewSuboptions').empty()
+        $('#viewSuboptions').append(reloadButton)
+    })
+    var historicalButton = $('<button></button>').addClass('btn btn-primary btn-block').html('Historical').on('click', function(e) {
+        e.preventDefault()
+    })
 
     buttonGroup.append(realTimeButton).append(historicalButton)
 
     var eventViewOptions = $('<div></div>').addClass('row-fluid')
     eventViewOptions.append(buttonGroup)
     eventViewOptions.append(
-        $('<div></div>').addClass('span11').addClass('well').html('Do stuff here.')
+        $('<div></div>').addClass('span11 well').attr('id', 'viewSuboptions').html('Do stuff here.')
     )
 
-    // Build our actual event table.
+    $('#eventViewOptions').empty()
+    $('#eventViewOptions').append(eventViewOptions)
+
+    // Set our real-time or historical button based on what we're loading.
+    if(realTime) {
+        realTimeButton.click()
+    } else {
+        historicalButton.click()
+    }
+}
+
+function populateEventsTable(events) {
+    // Build our table.
     var eventTable = $('<table></table>').addClass('table').attr('id', 'eventTable')
     var eventTableBody = $('<tbody></tbody>')
 
@@ -177,18 +217,42 @@ function populateEvents(serverName, events, realTime) {
     eventTable.append(eventTableHeader)
     eventTable.append(eventTableBody)
 
-    $('#eventViewOptions').empty()
-    $('#eventViewOptions').append(eventViewOptions)
-
     $('#eventTable').empty()
     $('#eventTable').append(eventTable)
+}
 
-    // Set our real-time or historical button based on what we're loading.
-    if(realTime) {
-        realTimeButton.click()
-    } else {
-        historicalButton.click()
-    }
+function populateEvents(serverName, events, realTime) {
+    // Build our events header - server name, time, etc.
+    populateEventsHeader(serverName)
+
+    // Populate the events view options area.
+    populateEventViewOptions(realTime)
+
+    // Populate our events table.
+    populateEventsTable(events)
+}
+
+function refreshServerData() {
+    // Clear out any errors, just to be safe.
+    $("#errors").empty()
+
+    // Clear out the existing event table.
+    $('#eventTable').empty()
+
+    // Get the latest data for the given server.
+    pullLatestData(currentServer, function(serverName, data) {
+        // Update the time of the snapshot.
+        updateEventsHeaderTime()
+
+        // Populate only the event table itself.
+        populateEventsTable(data.events)
+    })
+}
+
+function clearEventContent() {
+    $('#eventsHeader').empty()
+    $('#eventViewOptions').empty()
+    $('#eventTable').empty()
 }
 
 function showErrorMessage(title, message, appends) {
